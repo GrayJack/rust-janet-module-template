e12f92e3bd9d150fb7984e97892eee4684e4ae61
GrayJack <gr41.j4ck@gmail.com>
2021-06-02 01:35:47 -0300

    Update to latest janetrs

--- README.md
+++ README.md
@@ -1,2 +1,20 @@
 # rust-janet-module-template
-A template of a native Janet module written in Rust
+
+A template of a native Janet module written in Rust.
+
+## Usage example
+
+### Instalation
+
+```sh
+$ jpm install https://github.com/GrayJack/rust-janet-module-template.git
+```
+
+### Using inside Janet
+
+```clojure
+(use template)
+
+(hello)
+(chars "Test")
+```
--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,4 @@
-use janetrs::{
-    janet_fn, janet_mod, jpanic,
-    types::{Janet, JanetBuffer, JanetString, JanetTuple, JanetType},
-    util::check_fix_arity,
-};
+use janetrs::{janet_fn, janet_mod, jpanic, util::check_fix_arity, Janet, JanetTuple, TaggedJanet};
 
 #[janet_fn]
 pub fn rust_hello(args: &mut [Janet]) -> Janet {
@@ -14,19 +10,9 @@ pub fn rust_hello(args: &mut [Janet]) -> Janet {
 #[janet_fn]
 pub fn chars(args: &mut [Janet]) -> Janet {
     check_fix_arity(args, 1);
-    match args[0].kind() {
-        JanetType::Buffer => args[0]
-            .unwrap::<JanetBuffer>()
-            .unwrap()
-            .chars()
-            .collect::<JanetTuple>()
-            .into(),
-        JanetType::String => args[0]
-            .unwrap::<JanetString>()
-            .unwrap()
-            .chars()
-            .collect::<JanetTuple>()
-            .into(),
+    match args[0].unwrap() {
+        TaggedJanet::Buffer(b) => b.chars().collect::<JanetTuple>().into(),
+        TaggedJanet::String(s) => s.chars().collect::<JanetTuple>().into(),
         _ => jpanic!(
             "bad slot #0, expected string|buffer, got {}",
             args[0].kind()
